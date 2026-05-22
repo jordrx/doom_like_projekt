17c7495c4694adc64474946e5b1e8409d1f700f7
U-GIAA75\jhilmi <jhilmi@APIDV017.giaa.asso>
2026-05-22 15:01:28 +0200

    feat: init rotation + movement + clock/dt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include <CMath>
 
 const int MAP_W = 8;
 const int MAP_H = 8;
+
+const float MOVE_SPEED = 3.0f;
+const float ROTATE_SPEED = 3.0f;
+
 
 int map[MAP_H][MAP_W] = {
     {1, 1, 1, 1, 1, 1, 1, 1},
@@ -19,18 +24,46 @@ int map[MAP_H][MAP_W] = {
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Ma fenetre SFML 3");
+    
+    float playerX = 3.0f;
+    float playerY = 3.0f;
+    float angle = 0.0f;
+
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {
+        float dt = clock.restart().asSeconds();
+        window.setTitle("x: " + std::to_string(playerX) + " y: " + std::to_string(playerY) + " angle: " + std::to_string(angle));
+
         while (auto event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+        
+    //Rotation et déplacement du joueur
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        angle -= ROTATE_SPEED * dt;
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        angle += ROTATE_SPEED * dt;
+
+    float dx = std::cos(angle) * MOVE_SPEED * dt;
+    float dy = std::sin(angle) * MOVE_SPEED * dt;
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+    {
+        if (map[int(playerY)][int(playerX + dx)] == 0)
+            playerX += dx;
+        if (map[int(playerY + dy)][int(playerX)] == 0)
+            playerY += dy; 
+    }
 
         window.clear(sf::Color::Blue);
         window.display();
     }
+
 
     return 0;
 }
